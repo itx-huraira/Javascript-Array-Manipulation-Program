c7633eb21393f40ff9f04316f22fc4f710401073
Abu Huraira <127379942+itx-huraira@users.noreply.github.com>
2023-03-18 12:08:48 +0500

    Add files via upload

---
+++ array.ts
@@ -0,0 +1,8 @@
+let fruits = ['apple','Banana','Strawberry','orange','graps','cherry'];
+console.log(fruits);
+fruits.push('ananas','Kiwi'); //add end of array
+console.log(fruits);
+fruits.unshift('mango') //add beginning of array
+console.log(fruits);
+fruits.splice(2,1,'peach');
+console.log(fruits);
